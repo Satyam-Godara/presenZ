debb2bd664c126d93f652f6b1bd7ea8597283c1b
Satyam Godara <satyamgodara2005@gmail.com>
2026-08-16 22:43:28 +0530

    update group schema

--- models/Group.js
+++ models/Group.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
     // subjectId : {type: mongoose.Schema.Types.ObjectId , ref:'Subject' , required:true},
-    name : {type:String, required:true, unique:true, trim:true}
+    name : {type:String, required:true, unique:true, trim:true},
+    code : {type:String,required:true, unique:true, trim:true}
 },{timestamps:true});
 
 module.exports = mongoose.model('Group',groupSchema);
